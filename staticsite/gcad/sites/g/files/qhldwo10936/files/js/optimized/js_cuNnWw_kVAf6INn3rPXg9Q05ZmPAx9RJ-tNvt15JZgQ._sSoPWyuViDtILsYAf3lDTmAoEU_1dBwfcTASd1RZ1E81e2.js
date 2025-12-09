f0415583aef975da152b94a1914635a5dc51c1fa
aditilondhe2407 <aditi.udayakumar@gds.ey.com>
2025-12-09 12:16:54 +0530

    cleanup

--- staticsite/gcad/sites/g/files/qhldwo10936/files/js/optimized/js_cuNnWw_kVAf6INn3rPXg9Q05ZmPAx9RJ-tNvt15JZgQ._sSoPWyuViDtILsYAf3lDTmAoEU_1dBwfcTASd1RZ1E81e2.js
+++
@@ -1,9 +0,0 @@
-(function (Drupal, OtsukaPCM) {
-    Drupal.behaviors.cmpInitIframes = {
-      attach: (context) => {
-        once("cmpInitIframes", "iframe", context).forEach((iframe) => {
-          OtsukaPCM.initIframe(iframe);
-        });
-      },
-    };
-})(Drupal, OtsukaPCM);
